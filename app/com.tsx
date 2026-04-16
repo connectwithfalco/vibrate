b4ffa94c9d7034bdfaa0a06c0af6d5ec617c1ad2
connectwithfalco <faisal.saleem@universalplus.technology>
2026-04-16 22:40:40 +0500

    code

--- app/com.tsx
+++ app/com.tsx
@@ -7,6 +7,7 @@ export default function Page2() {
     useEffect(() => {
         setTimeout(() => {
             Haptics.impact({ style: ImpactStyle.Heavy }); // ✅ works after delay
+            console.log('111111');
         }, 4000);
     }, [])
 
